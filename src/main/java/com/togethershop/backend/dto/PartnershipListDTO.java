db9140da3e74c5d64172e27c9024fbfd153c9f74
GuBon <rnqhsrud01@gmail.com>
2025-09-09 21:05:07 +0900

    feat : 제휴 매장 목록 DTO #12

---
+++ src/main/java/com/togethershop/backend/dto/PartnershipListDTO.java
@@ -0,0 +1,23 @@
+package com.togethershop.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PartnershipListDTO {
+    private Long businessId;
+    private String businessName;
+    private String businessCategory;
+    private String address;
+    private Double latitude;
+    private Double longitude;
+    private Double togetherIndex;
+    private String profileImageUrl;
+    private String description;
+    private String collaborationCategory;
+}
